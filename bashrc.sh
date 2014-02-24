7eeae96106b495a008cc89802d002aca0edeab42
lukedeo <lukepercivaldeoliveira@gmail.com>
2014-02-24 11:26:04 -0500

    initial

---
+++ bashrc.sh
@@ -0,0 +1,60 @@
+# .bashrc
+
+# Source global definitions
+if [ -f /etc/bashrc ]; then
+	. /etc/bashrc
+fi
+
+# User specific aliases and functions
+#. /home/hep/share/.hep_bash_profile
+
+# -- ATLAS Specific shit
+source ~dhg3/setup/hep_bash.sh 
+shopt -s expand_aliases
+export ATLAS_LOCAL_ROOT_BASE=/home/hep/share/app/atlas/ATLASLocalRootBase
+alias setupAtlas='source ${ATLAS_LOCAL_ROOT_BASE}/user/atlasLocalSetup.sh'
+setupAtlas --quiet
+
+
+# -- specific python distro 
+
+export PATH=/home/hep/share/app/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/hep/share/app/lib
+
+
+# -- set up the big bitch thats ROOT
+ROOTBASE=/home/hep/baker/dhg3/public/root_5_34_09_roofit
+export ROOTSYS=${ROOTBASE}
+export PATH+=:${ROOTBASE}/bin
+export LD_LIBRARY_PATH=${ROOTBASE}/lib/root:$LD_LIBRARY_PATH
+
+# --- git 
+GIT_DIR=~dhg3/public/git-1.8.5.4
+export PATH=${PATH}:${GIT_DIR}/bin
+export MANPATH=${MANPATH}:${GIT_DIR}/share
+. ${GIT_DIR}/etc/git-completion.bash
+
+
+# -- Load some better stuff
+module load Compilers/Compilers/GCC/4.8.1
+module load Libraries/Eigen/3.2.0
+
+if [[ -z "$(find $HOME | grep "gitconfig")" ]]; then
+	git config --global --add color.ui true
+fi
+
+
+PYROOT_DIR=$(root-config --libdir)
+
+
+# --- atlas / blas 
+LOOK_MA_ATLAS=/lustre/home/client/apps/fas/Libs/ATLAS/atlas-3.8.4-gcc/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${LOOK_MA_ATLAS}
+
+complete -u finger
+
+export PYTHONPATH+=$PYROOT_DIR
+
+
+source $HOME/prefs/deoliveira-omega-prefs.sh
+
---
+++ deoliveira-omega-prefs.sh
@@ -0,0 +1,75 @@
+export CLICOLOR=1
+export GREP_OPTIONS='--color=auto'
+export LSCOLORS=exfxcxdxBxegedabagacad
+
+alias pushit='git push origin HEAD'
+alias pullit='git pull origin HEAD'
+
+alias gc='git commit -am'
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gb='git branch'
+alias ga='git add'
+alias gm='git merge --no-ff'
+
+mark(){
+    b=`pwd`
+    echo "Set jump-point to current directory." 
+}
+
+go(){
+    jumppoint=`pwd`
+    cd $b 
+    pwd
+    b=$jumppoint
+    unset jumppoint
+}
+
+alias marked='$(echo $b)'
+
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias root='root -l'
+
+alias up='cd .. && ls'
+alias tree="tree -C"
+
+alias all="ls -alh"
+alias reloadbash='source $HOME/.bashrc'
+alias editbash='nano $HOME/.bashrc'
+
+alias findme='qstat hep -t | grep --color=auto "lpd7"'
+
+export TERM=xterm-color
+
+
+
+
+export PS_BU=$PS1
+export PS_ON="\033[1;31mWorking Dir: \[\033[37;1m\][\w]:\[\033[0m\] \n\[\033[1;32m\]   \342\224\224\342\224\200\342\224\200[\[\033[0m\]\[\033[1;31m\]\@\[\033[0m\]\[\033[1;32m\] \u]\$ \[\033[0m\]"
+
+export PS_LUX="\[\e[00;32m\]\u@\h\[\e[0m\]\[\e[00;37m\] \[\e[0m\]\[\e[00;36m\][\w]\[\e[0m\]\[\e[00;37m\]$ \[\e[0m\]"
+
+prompt-on(){
+    export PS1=$PS_ON
+}
+prompt-off(){
+    export PS1=$PS_BU
+}
+prompt-lux(){
+    export PS1=$PS_LUX
+}
+
+prompt-lux
+
+
+alias findme='qstat hep -t | grep --color=auto "lpd7"'
+
+
+
+
+
+
+
